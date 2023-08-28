b6527ee7bf730d4b852a1735e62f5cedfb0103be
sol lobo <lobosol62@gmail.com>
2023-08-28 20:41:12 -0300

    agrego modal a editar y funcion

--- js/administracion.js
+++ js/administracion.js
@@ -1,3 +1,8 @@
+const editarproduct = document.querySelector("#editProduct")
+const editarproductId = document.querySelector("#editProductId")
+const editardescripcion = document.querySelector("#editDescripcion")
+const editarimg = document.querySelector("#editImg")
+
 let crearListaProductosAdmin = () => {
   let listaProductos = leerProductos();
   let lugarhtml = document.getElementById("listaProductos");
@@ -12,10 +17,11 @@ let crearFilaProducto = (producto) => {
             <td id="producto-id-${producto.productId}">${producto.productId}</td>
             <td id="producto-nombre-${producto.productId}">${producto.name}</td>
             <td id="producto-descripcion-${producto.productId}">${producto.descrip}</td>
-            <td id="producto-botones-${producto.productId}"><button class="btn btn-danger me-1" onclick="eliminarProducto(${producto.productId})">Eliminar</button><button class="btn btn-info">Modificar</button></td>
+            <td id="producto-botones-${producto.productId}"><button class="btn btn-danger me-1" onclick="eliminarProducto(${producto.productId})">Eliminar🗑️</button><button class="btn btn-info" id ="${product.productId}" data-bs-toggle="modal" data-bs-target="#idModal" >Editar ✏️</button></td>
         </tr>
     `;
 };
+
 
 let eliminarProducto = (productId) => {
   eliminarProductoLocalStorage(productId);
@@ -40,3 +46,19 @@ let agregarProducto = (event) => {
 };
 
 crearListaProductosAdmin();
+
+//! editar tarjetas
+  function modificarProducto() {
+    event.preventDefault();
+    const modificarProduct = new product(
+      editarproduct.value,
+      editarproductId.value,
+      editardescripcion.value,
+      editarimg.value
+     )
+     const arrayProductos = leerProductos();
+     arrayProductos.splice ( parseInt(modificarProduct.productId) -1, 1, modificarProduct); 
+     localStorage.setItem("productos", JSON.stringify(arrayProductos));
+     location.reload()
+   
+    }
