08c90394aabd1227faf2b1548ec3a9cec972094f
JuaniBurgos <juanburgos86b@gmail.com>
2023-08-29 23:21:25 -0300

    Primera parte del Carrito de compra

---
+++ js/detalleCarrito.js
@@ -0,0 +1,11 @@
+class ProductCarrito {
+    constructor(idProduct, name ,price, cantidad){
+        this.idProduct = idProduct;
+        this.name= name;
+        this.price= price;
+        this.cantidad= cantidad;
+
+
+    }
+}
+
